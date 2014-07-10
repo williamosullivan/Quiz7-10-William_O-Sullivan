44574fb4069ef6d256dda341d930bd5a84637c57
William O'Sullivan <williamosullivan@gmail.com>
2014-07-10 13:13:29 -0400

    Problem 3 Coded and Tested
    Everything working for 3rd Question

---
+++ Quiz7-10-William_O-Sullivan/Quiz7-10-William_O-Sullivan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz7_10_William_O_Sullivan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(2147483647);
+
+            EvenOrOdd(number);
+            Console.ReadLine();
+        }
+
+        private static void EvenOrOdd(int number)
+        {
+            Console.WriteLine(number);
+            Console.ReadLine();
+
+            if (number % 2 == 0)
+                Console.WriteLine("Even");
+            else
+                Console.WriteLine("Odd");
+        }
+    }
+}
